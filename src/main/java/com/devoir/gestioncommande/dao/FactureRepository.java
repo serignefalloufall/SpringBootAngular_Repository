591e0ba0049591bbdd69668e1288ad022d4857fa
serignefalloufall <fallfallou37@gmail.com>
2020-08-08 15:49:35 +0000

    Som change

--- src/main/java/com/devoir/gestioncommande/dao/FactureRepository.java
+++ src/main/java/com/devoir/gestioncommande/dao/FactureRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FactureRepository extends JpaRepository<Facture, Long> {
+public interface FactureRepository extends JpaRepository<Facture, Long>{
 }
--- src/main/java/com/devoir/gestioncommande/dao/PaiementRepository.java
+++ src/main/java/com/devoir/gestioncommande/dao/PaiementRepository.java
@@ -1,8 +1,10 @@
 package com.devoir.gestioncommande.dao;
 
+import com.devoir.gestioncommande.model.Facture;
+import com.devoir.gestioncommande.model.Paiement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaiementRepository extends JpaRepository {
+public interface PaiementRepository extends JpaRepository<Paiement, Long>{
 }
